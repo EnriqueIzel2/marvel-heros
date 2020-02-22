79427259e1ee1fcd1f06976ac0388b02fa740e07
EnriqueIzel2 <eleaoizel@gmail.com>
2020-02-22 18:27:10 -0400

    add route to About screen

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ const Stack = createStackNavigator();
 
 import Home from './Screens/Home';
 import Description from './Screens/Description';
+import About from './Screens/About';
 
 export default function Routes() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Heros" component={Home} />
       <Stack.Screen name="Description" component={Description} />
+      <Stack.Screen name="About" component={About} />
     </Stack.Navigator>
   );
 }
