a15bb381e732d18a368b459688177c37674f5368
EnriqueIzel2 <eleaoizel@gmail.com>
2020-02-22 18:27:44 -0400

    creating About screen

---
+++ src/Screens/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const About = () => (
+  <View>
+    <Text>Criador para aprender a versão 5 do react navigation</Text>
+  </View>
+);
+
+export default About;
