db1d42abb34d9bee71647e8b87682a485cc61fef
EnriqueIzel2 <eleaoizel@gmail.com>
2020-02-24 15:20:23 -0400

    creating description screen

--- src/Screens/Description/index.js
+++ src/Screens/Description/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import {ScrollView, Text, Image} from 'react-native';
 
-import {View, Text} from 'react-native';
-// import { Container } from './styles';
+import styles from './styles';
 
-const Description = () => (
-  <View>
-    <Text>Description</Text>
-  </View>
-);
+const Description = ({route}) => {
+  const {hero} = route.params;
+
+  function hasDescription() {
+    return hero.description ? true : false;
+  }
+
+  function renderDescription() {
+    return <Text style={styles.heroDescription}>{hero.description}</Text>;
+  }
+
+  function renderMessageError() {
+    return (
+      <Text style={styles.heroDescription}>
+        Sorry ! this character has no description
+      </Text>
+    );
+  }
+
+  return (
+    <ScrollView>
+      <Image
+        source={{uri: `${hero.thumbnail.path}.${hero.thumbnail.extension}`}}
+        style={styles.heroImage}
+      />
+      <Text style={styles.heroName}>{hero.name}</Text>
+      {hasDescription() ? renderDescription() : renderMessageError()}
+    </ScrollView>
+  );
+};
 
 export default Description;
